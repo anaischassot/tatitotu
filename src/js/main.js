309b308fa141eda0b22e13478b10bfefe5bef897
anaischassot <Anais.Chassot@studentfr.ch>
2024-04-24 17:00:10 +0200

    codage js

--- src/js/main.js
+++ src/js/main.js
@@ -1,40 +1,41 @@
-var burger = document.querySelector(".burger");
+var burger = document.querySelector(".menu-burger");
 var menu = document.querySelector(".menu");
 var toggleMenu = function toggleMenu() {
   menu.classList.toggle("is-active");
   burger.classList.toggle("is-active");
 };
 burger.addEventListener("click", toggleMenu);
-var checkAllTrigger = document.querySelector(".select-all");
-var checkboxes = document.querySelectorAll(".radio-placeholder");
-console.log(checkAllTrigger, checkboxes);
+
+var checkAllTrigger = document.querySelector(".card-times-text-select");
+var checkboxes = document.querySelectorAll(".checkbox");
 var checkAll = function () {
   checkboxes.forEach((checkbox) => {
     checkbox.checked = true;
   });
 };
 checkAllTrigger.addEventListener("click", checkAll);
-var sections = document.querySelectorAll(".times");
-sections.forEach((times) => {
-  //[on mettra ici le code à effectuer pour chaque section//]
-});
-sections.forEach((times) => {
-  var checkAllTrigger = times.querySelector(".check-all");
-  var checkboxes = times.querySelectorAll(".checkbox");
-  var checkAll = function () {
-    checkboxes.forEach((checkbox) => {
-      checkbox.checked = true;
-    });
-  };
 
-  checkAllTrigger.addEventListener("click", checkAll);
-});
+var uncheckAllTrigger = document.querySelector(".card-times-text-uncheck");
+var uncheckAll = function () {
+  checkboxes.forEach((checkbox) => {
+    checkbox.checked = false;
+  });
+};
+uncheckAllTrigger.addEventListener("click", uncheckAll);
+
 var verbs = ["aimer", "faire", "voyager", "réféchir", "boire"];
 var textfield = document.querySelector(".textfield");
-var suggestions = document.querySelector(".suggestions");
+var suggestions = document.querySelector(".card-verb-suggestions");
 var autocomplete = function () {
   console.log("test");
   var items = verbs.filter((value) => value.includes(textfield.value));
   suggestions.innerHTML = items.join(", ");
 };
 textfield.addEventListener("keyup", autocomplete);
+
+var options = document.querySelector(".card-options-print input");
+var hiddenOptions = document.querySelector(".card-options-hidden");
+var toggleOptions = function toggleOptions() {
+  hiddenOptions.classList.toggle("is-active");
+};
+options.addEventListener("change", toggleOptions);
